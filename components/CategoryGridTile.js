c469180cc32f80db23306f39bd989c9d8ba2328f
Aleksey Belchenkov <u608110@gmail.com>
2020-07-02 14:03:28 +0300

    Fix the shadows

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -39,7 +39,10 @@ const styles = StyleSheet.create({
         margin: 15,
         borderRadius: 10,
         height: 150,
-        overflow: 'hidden'
+        overflow: Platform.OS === 'android' && Platform.Version >= 21
+            ? 'hidden'
+            : 'visible',
+        elevation: 5
     },
     container: {
         flex: 1,
@@ -50,7 +53,6 @@ const styles = StyleSheet.create({
         shadowOpacity: .26,
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 10,
-        elevation: 3,
         padding: 15,
     },
     title: {
