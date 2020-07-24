87424e0ef3d53287824ea2993387b2955dd9d7c5
Aleksey Belchenkov <u608110@gmail.com>
2020-07-24 16:14:04 +0300

    Dispatching Filtet Actions

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useSelector } from "react-redux";
+import { View, StyleSheet } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
 import MealList from "../components/MealList";
+import DefaultText from "../components/DefaultText";
 
 const CategoryMealScreen = ({ navigation }) => {
     const catId = navigation.getParam('categoryId');
     const meals = useSelector(state => state.meals.filteredMeals);
 
     const displayedMeals = meals.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+
+    if (displayedMeals.length === 0) {
+        return <View style={styles.content}>
+            <DefaultText>No meals found, maybe check your filters?</DefaultText>
+        </View>
+    }
 
     return <MealList listData={displayedMeals} navigation={navigation} />;
 };
@@ -21,5 +29,13 @@ CategoryMealScreen.navigationOptions = ({ navigation }) => {
         headerTitle: selectedCategory.title,
     };
 };
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default CategoryMealScreen;
